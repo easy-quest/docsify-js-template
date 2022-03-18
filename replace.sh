efe98a408c17751893f34f57f384468ccdfb4721
Ivan Yastrebov <easy.quest@vk.com>
2022-03-19 02:03:56 +0300

    Linux localhost 4.14.116 #1 SMP PREEMPT Tue Feb 15 21:41:21 CST 2022 aarch64 Android

---
+++ replace.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# Replacement utility script.
+#
+# Следуйте инструкциям в README.md чтобы использовать этот скрипт, а затем удалить его, когда вы закончите.
+
+set -e
+cd docs
+
+## ИЗМЕНИТЕ ИХ С ПОМОЩЬЮ ВАШИХ ЗНАЧЕНИЙ
+
+MY_TITLE='Foo Bar'
+MY_SUBTITLE='Foos the bar all the buzz'
+MY_USERNAME='MyGitHubUsername'
+MY_PROJECT_SLUG='foo-bar'
+
+## НЕ ИЗМЕНЯТЬ ПОСЛЕ СТРОКИ ВЫШЕ
+
+MY_REPO="$MY_USERNAME\/$MY_PROJECT_SLUG"
+MY_GETTING_STARTED="$MY_PROJECT_SLUG"
+
+ORIGINAL_TITLE='DocsifyJS Template'
+ORIGINAL_SUBTITLE='Начальный шаблон для сайта документов на основе markdown'
+ORIGINAL_REPO='easy-quest\/docsify-js-template'
+ORIGINAL_GETTING_STARTED='docsifyjs-template'
+
+PATTERN="s/$ORIGINAL_TITLE/$MY_TITLE/g
+s/$ORIGINAL_REPO/$MY_REPO/g
+s/$MY_GETTING_STARTED/$ORIGINAL_GETTING_STARTED/g
+s/$ORIGINAL_SUBTITLE/$MY_SUBTITLE/g"
+
+echo 'PATTERN'
+echo "$PATTERN"
+
+sed -i'' "$PATTERN" *
+
+echo "Done!"
